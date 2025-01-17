813dcbe7ea43c63b71a3546549d29b01d900532c
Yasindu <Yasindu@YASA>
2025-01-17 22:38:01 +0530

    Add project files.

---
+++ StudentAPI/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string GetGreering()
+        {
+            return "Hello there!";
+        }
+        //CRUD
+        //Create - a new record - POST
+        //Read - retieve a single/ listof record(s) - GET
+        //Update - modify an exisitng record - PUT
+        //Delete - remove an exisiting record - DELETE
+    }
+}
